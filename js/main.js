89a671a270493d38d9bf5b20df7c4a94f5932939
Andrew Lin <andrew.61523@gmail.com>
2022-03-19 17:36:55 -0400

    About-Me Wrapping

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("Navigation-Bar-Container").style.background = "white";
+    document.getElementById("Navigation-Bar-Container").style.boxShadow = "3px 3px 10px rgba(0, 0, 0, 0.1)";
     document.getElementById("logo-text").style.color = "black";
     document.getElementById("home-text").style.color = "black";
     document.getElementById("about-text").style.color = "black";
@@ -10,6 +11,7 @@ function scrollFunction() {
     document.getElementById("resume-text").style.color = "black";
   } else {
     document.getElementById("Navigation-Bar-Container").style.background = "none";
+    document.getElementById("Navigation-Bar-Container").style.boxShadow = "none";
     document.getElementById("logo-text").style.color = "#d1d4d4";
     document.getElementById("home-text").style.color = "white";
     document.getElementById("about-text").style.color = "white";
