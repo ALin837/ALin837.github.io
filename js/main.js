111d19ca01059434b3779c2d78af41857f3388a6
Andrew Lin <andrew.61523@gmail.com>
2022-03-19 08:08:29 -0400

    Fixed Background and Added scroll-to-View

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("Navigation-Bar-Container").style.background = "#5B628F";
+    document.getElementById("Navigation-Bar-Container").style.background = "#545b87";
   } else {
     document.getElementById("Navigation-Bar-Container").style.background = "none";
   }
+}
+
+
+const sections = document.getElementsByClassName("scroll_to");
+console.log(sections)
+for (var i = 0; i < sections.length; i++) {
+  let element = sections[i];
+  element.addEventListener("click", () => {
+      const el = document.getElementById(element.getAttribute("data-link"));
+      el.scrollIntoView({behavior:"smooth", block:"start"})
+  })
 }
