1848dbc7ce2d738877c9589bc4e07c701805579a
Andrew Lin <andrew.61523@gmail.com>
2022-03-20 09:48:33 -0400

    nav bar color fixes

--- js/main.js
+++ js/main.js
@@ -4,26 +4,38 @@ function scrollFunction() {
     document.getElementById("Navigation-Bar-Container").style.background = "white";
     document.getElementById("Navigation-Bar-Container").style.boxShadow = "3px 3px 10px rgba(0, 0, 0, 0.1)";
     document.getElementById("logo-text").style.color = "black";
-    document.getElementById("home-text").style.color = "black";
-    document.getElementById("about-text").style.color = "black";
-    document.getElementById("experience-text").style.color = "black";
-    document.getElementById("projects-text").style.color = "black";
-    document.getElementById("resume-text").style.color = "black";
+    document.getElementById("hamburger").style.color = "black"
+    let resume = document.getElementsByClassName("ResumeClass");
+    for (let i = 0; i < resume.length; i++) {
+      let element = resume[i];
+      element.style.color="black";
+    }
+    let collection = document.getElementsByClassName("scroll_to");
+    for (let i = 0; i < collection.length; i++) {
+      let element = collection[i];
+      element.style.color="black";
+    }
+
   } else {
     document.getElementById("Navigation-Bar-Container").style.background = "none";
     document.getElementById("Navigation-Bar-Container").style.boxShadow = "none";
     document.getElementById("logo-text").style.color = "#d1d4d4";
-    document.getElementById("home-text").style.color = "white";
-    document.getElementById("about-text").style.color = "white";
-    document.getElementById("experience-text").style.color = "white";
-    document.getElementById("projects-text").style.color = "white";
-    document.getElementById("resume-text").style.color = "white";
+    document.getElementById("hamburger").style.color =  "#d1d4d4";
+    let resume = document.getElementsByClassName("ResumeClass");
+    for (let i = 0; i < resume.length; i++) {
+      let element =resume[i];
+      element.style.color="white";
+    }
+    let collection = document.getElementsByClassName("scroll_to")
+    for (let i = 0; i < collection.length; i++) {
+      let element = collection[i];
+      element.style.color="white";
+    }
   }
 }
 
 
 const sections = document.getElementsByClassName("scroll_to");
-console.log(sections)
 for (var i = 0; i < sections.length; i++) {
   let element = sections[i];
   element.addEventListener("click", () => {
