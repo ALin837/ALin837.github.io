0fb3b4004e9b6de5afae65da3678f8f132cc3e40
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2022-04-25 20:08:12 -0400

    Type Writer

--- js/main.js
+++ js/main.js
@@ -166,3 +166,76 @@ window.addEventListener("scroll", ()=> {
 })
 
 
+
+
+class Typewriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+
+   type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove characters
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add charaters
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = this.txt;
+
+    // Initial Type Speed
+    let typeSpeed = 120;
+
+    if (this.isDeleting) {
+      // Increase speed by half when deleting
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+
+
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const element = document.getElementById('type-writer-content')
+  const words = ["I'm Andrew Lin.", "I'm a Developer."]
+  const wait = 3000;
+  // Init TypeWriter
+  
+  new Typewriter(element, words, wait);
+
+}
